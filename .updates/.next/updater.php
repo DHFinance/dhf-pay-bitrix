f293498d7293b3ef44a926bc416be25a6fa666c2
Marat Shamshutdinov <marat@citrus-soft.ru>
2022-06-10 09:08:54 +0300

    hotfix

--- .updates/.next/updater.php
+++ .updates/.next/updater.php
@@ -6,7 +6,7 @@ use Bitrix\Main\Loader;
 
 if (Loader::includeModule($updater->moduleID)
 	&& $updater->CanUpdateDatabase()) {
-	$updater->Query("alter table citrus_dhfi_payments add DATE_UPDATE timestamp default CURRENT_TIMESTAMP on update set CURRENT_TIMESTAMP not null after ID");
+	$updater->Query("alter table citrus_dhfi_payments add DATE_UPDATE timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null after ID");
 	$updater->Query("alter table citrus_dhfi_payments add DATE_CREATE timestamp default CURRENT_TIMESTAMP not null after DATE_UPDATE");
 	$updater->Query("alter table citrus_dhfi_payments change ENTITY_ID ACCOUNT_NUMBER varchar(255) not null");
 	$updater->Query("alter table citrus_dhfi_payments change ENTITY_TYPE REGISTRY varchar(255) not null");
